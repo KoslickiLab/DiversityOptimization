919ddb72dee881c8d73a164069e35fb1fb2cfd7b
dkoslicki <david.koslicki@math.oregonstate.edu>
2020-03-05 10:46:51 -0600

    add help text to function #1

--- MatlabCode/src/MinDivLP.m
+++ MatlabCode/src/MinDivLP.m
@@ -1,4 +1,24 @@
 function x_star = MinDivLP(A_k_small, A_k_large, y_small, y_large, lambda, q)
+%MinDivLP
+% A basic, regularized version of the MinDivLP algorithm.
+% Call via:
+% x_star = MinDivLP(A_k_small, A_k_large, y_small, y_large, lambda, q)
+%
+% Parameters are:
+%   A_k_small is the [m_small, N]-sized sensing matrix
+%   A_k_large is the [m_large, N]-sized sensing matrix
+%   y_small is the data vector of size [m_small, 1]
+%   y_large is the data vector of size [m_large, 1]
+%   lambda is the regularization paramater (larger values indicated better
+%     fit to constraints, at the cost potentially higher execution time and 
+%     may lead to over-fitting if set too large. Typical value is 10000 or
+%     1000
+%   q is the parameter used in the MinDivLP algorithm. Must have 0<q<1,
+%     typically, q is set to something like q = 0.1
+%
+% Returns:
+% x_star: an [N, 1] vector
+
     % Generate the B matrix
     B = (A_k_large > 0);  % create the B matrix
     f = 1./(B'*y_large).^(1-q);  % weighting factor in MinDivLP, no noise in this y vector
@@ -9,4 +29,3 @@ function x_star = MinDivLP(A_k_small, A_k_large, y_small, y_large, lambda, q)
     x_star = lsqnonneg([f'; lambda*A_k_small], [0;lambda*y_small]);  % the regularized version of MinDivLP for noisy data
     x_star = x_star./sum(x_star);  % normalize
 end
-
