a3ea7f5656d17bdc49096b55a3c938e3445bdcda
Chase <cmcolbert@tamu.edu>
2020-05-25 09:23:00 -0500

    added multiprocessing and changed to write to csv throughout #2

--- PythonCode/experiments/Benchmark_speed.py
+++ PythonCode/experiments/Benchmark_speed.py
@@ -5,37 +5,39 @@ import time
 import sys
 import os
 import csv
-from numpy.linalg import norm
+from multiprocessing import Process
 from statistics import mean, median, stdev
 
 sys.path.append(os.path.abspath("../.."))  # make sure python knows where to find MinDivLP
 from PythonCode.src.MinDivLP import MinDivLP
 
+## Data writing information
+resultCSV = "results.csv"
+overwrite = True
+
 ## Variables for data acquisition
-data_dir = ""  # input directory containing full sensing matrices
+data_dir = "../data"  # input directory containing full sensing matrices
 no_c = False  # append "_no_c" to exclude reverse complement k-mers
 
 ## Parameters of the variables being tested
-k_sizes = [4, 6, 8, 10, 12]  # k_mer size for A_k_large
-col_counts = [10000, 1000, 100]  # numbers of included columns of A_k (in decreasing order)
-support_sizes = [25, 50, 75]  # support sizes of true_x
+k_sizes = [4, 6, 8, 10, 12, 13]  # k_mer size for A_k_large to be tested
+col_counts = [99322, 50000, 10000, 1000, 100]  # numbers of included columns of A_k (in decreasing order)
+support_sizes = [25, 50, 100, 500, 1000, 5000]  # support sizes of true_x
 N = 20  # iterations at each combination of k_size, num_cols, support_size
 
 ## Other parameters
 q = .1  # fixed, small q value s.t. 0<q<1
-const = 10000  # lambda
+lamb = 10000  # lambda
 
-results = list()
 
-for large_k in k_sizes:  # num_rows is 4^k_size
-
+## Define testing function to parallelize
+def test(large_k, col_counts, support_sizes, N, q, lamb, csvname):
     ## import the data, set small_k
-    small_k = 4  # smaller k-mer size
-
-    A_k = sio.loadmat('{0}/97_otus_subset.fasta_A_{1}{2}.mat'.format(data_dir, large_k, no_c * "_no_c"))
+    A_k = sio.loadmat('{0}/97_otus.fasta_A_{1}{2}.mat'.format(data_dir, large_k, no_c * "_no_c"))
     A_k_large = A_k['A_k']
 
-    A_k = sio.loadmat('{0}/97_otus_subset.fasta_A_{1}{2}.mat'.format(data_dir, small_k, no_c * "_no_c"))
+    small_k = 4  # smaller k-mer size
+    A_k = sio.loadmat('{0}/97_otus.fasta_A_{1}{2}.mat'.format(data_dir, small_k, no_c * "_no_c"))
     A_k_small = A_k['A_k']
 
     del A_k
@@ -46,11 +48,11 @@ for large_k in k_sizes:  # num_rows is 4^k_size
         A_k_large = A_k_large[:, 0:num_cols]
 
         for support_size in support_sizes:  # number of non-zero entries in the simulated ground truth
-            times = list()
-            l1errors = list()
-            l2errors = list()
 
-            for i in range(0, N):
+            support_size = min(support_size, A_k_small.shape[1])
+            times = list()
+
+            for _ in range(N):
                 ## Create the simulated ground truth
                 supp = rand.choice(range(0, support_size), size=(1, support_size), replace=False)  # location of the
                 # support
@@ -64,30 +66,40 @@ for large_k in k_sizes:  # num_rows is 4^k_size
 
                 # Noisy y-vectors
                 noise_eps = .00001  # size of noise to add
-                y_small_noise = (A_k_small @ true_x) + noise_eps * rand.random((A_k_small.shape[0], 1))
+                y_small_noise = y_small_true + noise_eps * rand.random((A_k_small.shape[0], 1))
                 y_small_noise = y_small_noise / sum(y_small_noise)
 
                 ## Noisy computations
                 start_time = time.time()
-                x_star = MinDivLP(A_k_small.toarray(), A_k_large, y_small_noise, y_large_true, const, q).reshape(
+                x_star = MinDivLP(A_k_small.toarray(), A_k_large, y_small_noise, y_large_true, lamb, q).reshape(
                     num_cols, 1)
                 times.append(time.time() - start_time)
 
-                ## Measure the reconstruction accuracy
-                l1errors.append(norm(x_star - true_x, 1))
-                l2errors.append(norm(x_star - true_x, 2))
+            ## Append csv file with results
+            with open(csvname, "a", newline="") as f:
+                writer = csv.writer(f)
+                writer.writerow(
+                    [4 ** large_k, num_cols, support_size, N, min(times), max(times), median(times), mean(times),
+                     stdev(times)])
 
-            info = np.array([4 ** large_k, num_cols, support_size, min(times), max(times), median(times), mean(times),
-                             stdev(times), mean(l1errors), mean(l2errors)])
+            print("({0}, {1}, {2})".format(large_k, num_cols, support_size))
 
-            results.append(info)
 
-    print('finished large_k = {}'.format(large_k))  # Print to ensure running
+if __name__ == '__main__':
 
-## Save summary statistics to a csv file for analysis
-with open("results.csv", "w", newline="") as f:
-    writer = csv.writer(f)
-    writer.writerow(
-        ['rows', 'columns', 'support size', 'min', 'max', 'median', 'mean', 'std dev', 'mean l1 error', 'mean l2 '
-                                                                                                        'error'])
-    writer.writerows(results)
+    if overwrite:
+        ## Save first row of csv
+        with open(resultCSV, "w", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow(
+                ['rows', 'columns', 'support size', 'N', 'min', 'max', 'median', 'mean', 'std dev'])
+
+    ## Parallelize for each value of large_k
+    processes = list()
+    for large_k in k_sizes:
+        p = Process(target=test, args=(large_k, col_counts, support_sizes, N, q, lamb, resultCSV))
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
