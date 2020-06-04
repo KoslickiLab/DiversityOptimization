76fd5636d8de0efe212734111ee8bc47b41deee9
Chase <cmcolbert@tamu.edu>
2020-06-04 13:56:23 -0500

    Added docstring, made variable names clearer, and fixed typos in ConvertXToBIOM.py #7

--- PythonCode/src/ConvertXToBIOM.py
+++ PythonCode/src/ConvertXToBIOM.py
@@ -2,30 +2,44 @@ import numpy as np
 from biom.table import Table
 
 ## For now only one sample in one environment with an estimate of population from read counts
-def convertToBiom(x, trainingfile, yfile, samplename):
+def convertToBiom(x, trainingfile, samplefile, samplename):
+    """ convertToBiom
+        Creates a BIOM table from population proportions, a reference database fasta file, and a sample fasta file
+        Call via:
+        table = convertToBiom(x, trainingfile, samplefile, samplename)
+        Parameters are:
+        x is the vector of population proportions
+        training file is the string location of the reference database fasta file (assumed to list OTU id after '>' for
+            the following sequence)
+        samplefile is the string location of the sample fasta file of the sample
+        samplename is the name of the given sample
 
+        Returns:
+        table: a BIOM table with estimates
+
+    """
     observe_ids = list()
 
-    sample_reads = 0
-    sequence_reads = list()
+    sample_len = 0
+    persequence_len = list()
 
     # Create OTU ids with those provided in FASTA
     with open(trainingfile, 'r') as file:
         for line in file:
             if line[0] == '>':
                 observe_ids.append(line[1:-1])
-            else:  # Count the number of mers in each full sequence
-                sequence_reads.append(len(line)-1)  # Accounting for '>'
+            else:  # Count the number of bases in each full sequence, i.e. the length of each sequence
+                persequence_len.append(len(line)-1)  # Accounting for '>'
 
-    with open(yfile, 'r') as file:
+    with open(samplefile, 'r') as file:
         for line in file:
-            if line[0] != '>':  # Count the number of mers in the population
-                sample_reads += (len(line)-1)  # Accounting for '>'
+            if line[0] != '>':  # Count the number of bases in the population
+                sample_len += (len(line)-1)  # Accounting for '>'
 
-    unit_reads = np.array(sequence_reads) @ x  # unit_reads is the count of reads per unit population
-    const = sample_reads / unit_reads  # Calculate the number of unit populations to reach the number of reads in the
-    # sample. Likely an underestimate; number of reads in the population should be greater than or equal to the
-    # number of reads in the sample
+    unit_len = np.array(persequence_len) @ x  # unit_len is the count of bases per normalized population
+    const = sample_len / unit_len  # Calculate the number of unit populations to reach the number of bases in the
+    # sample, sample_len. Likely an underestimate; number of bases in the population should be greater than or equal
+    # to the number of bases in the sample
 
     counts = np.round(const * x).reshape((-1, 1))
 
